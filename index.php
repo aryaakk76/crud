ccc7c8cb7b7f7b38c2fb19b9c7cf62f33036a135
aryaakk76 <wahyuaryanaputra@gmail.com>
2023-11-20 10:21:57 +0700

    ini crud

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compitable" content="IE=edge"> 
+    <meta name="viewport" content="widht=device-width, initial-scale=1.0"> 
+    <title>Document</title>
+</head>
+<body>
+    <h1>sistem informasi sekolah</h1>
+    <h2>MANJO</h2>
+    <a href="data_siswa.php">Daftar siswa</a><br>
+    <a href="tambah_siswa.php">pendaftaran</a>
+</body>
+</html>
+      
